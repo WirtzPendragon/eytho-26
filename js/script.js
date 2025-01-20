d0f12c312cf203f251c82aef3419cc3d5dfd2a91
wirtzpendragon <adityawiranta2018@gmail.com>
2025-01-20 19:05:46 +0800

    nav bar burger

--- js/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('scroll', () => {
-    const navLogo = document.querySelector('.nav-logo');
-
-    if (window.scrollY > 627){
-        navLogo.classList.remove('nav-filter');
-    } else {
-        navLogo.classList.add('nav-filter');
-    }
-});
